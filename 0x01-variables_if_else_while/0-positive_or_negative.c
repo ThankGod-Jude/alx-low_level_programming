8c3df581751a9931e48178e65340905143f96e3a
ofukeyz <ofu@gmail.com>
2023-07-16 13:03:04 -0700

    C weldone oo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main = Entry point
+ * main - Entry point
  * Description: 'Check for number if it is positive or negative'
  * Return: Always 0 (Success)
  */
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,14 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main = Entry point
+ * main - Entry point
  * Description: 'get the last digits of a number'
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
-        int ld;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -22,10 +22,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-        else
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
 	return (0);
 }
-
