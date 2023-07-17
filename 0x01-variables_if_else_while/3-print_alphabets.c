19c25270660bf40c831c03d490e2b827ef590b0a
ofukeyz <ofu@gmail.com>
2023-07-17 14:33:42 -0700

    C weldone oo

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,17 @@
 int main(void)
 {
 	int n = 97;
-	int q = 65;
+	int n = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
+		n++;
 	}
 	while (n <= 90)
 	{
 		putchar(n);
+		n++;
 	}
 	putchar('\n');
 	return (0);
