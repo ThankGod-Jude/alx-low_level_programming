24807f2b8300d731e9be021d3191ff99425a538e
ofukeyz <ofu@gmail.com>
2023-07-19 11:10:09 -0700

    C dey choke

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,27 @@
 #include "main.h"
 /**
- * print_sign - Determines if the input number
- * greater, equal or less than zero.
- *
- * @n: The input number is an integer.
- *
- * Return: 1 is greater than zero. 0 is zero.
- * -1 is less than zero
+ * print_sign - Prints the sign of a number
+ * @n: The number to be checked
+ * Return: 1 for positive num, -1 for negative number or zero for anything else
  */
+
 int print_sign(int n)
+
 {
+
 	if (n > 0)
 	{
-		_putchar(43);
-		return (1);
+	_putchar(43);
+	return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(45);
-		return (-1);
+	_putchar(45);
+	return (-1);
 	}
 	else
 	{
-		_putchar(0);
-		return (0);
+	_putchar(48);
+	return (0);
 	}
-	_putchar('\n');
 }
