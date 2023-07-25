b1694f8b74c824526a0e3e481bdebe22dfb96659
ofukeyz <ofu@gmail.com>
2023-07-24 18:52:06 -0700

    C pointers!!!

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - print n elements of an arraqy of integers
+ * @a: array to be used
+ * @n: number of elements to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
